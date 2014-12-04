8c358ee8e86f9a36a3d961f9ef4e1fb68f85a977
Kyle Florence <kyle.florence@gmail.com>
2014-12-04 13:58:18 -0800

    Minor cleanup

--- tasks/clean.js
+++ tasks/clean.js
@@ -1,11 +1,9 @@
 var del = require("del");
-var gulp = require("gulp");
 
 /**
  * Removes files and/or folders matching the glob.
  *
  * @param {String} options.glob Glob pattern to match
- * @param {String} [options.name] Postfix :name
  */
 module.exports = function(options) {
   return function(cb) {
